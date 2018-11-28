41020231808aa756bb6889752772472f932b7fcb
Douglas Lundy <douglaslundy@gmail.com>
2018-11-28 14:39:58 -0200

    inserido botao mapa + funcionalidades

---
+++ src/main/java/br/com/dlsistemas/servicesgeo/controllers/GeolocalizacaoController.java
@@ -0,0 +1,13 @@
+package br.com.dlsistemas.servicesgeo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GeolocalizacaoController {
+	
+	@GetMapping("/geolocalizacao/iniciarpesquisa")
+	public String inicializarPesquisa() {
+		return "geolocalizacao/pesquisar";
+	}
+}
