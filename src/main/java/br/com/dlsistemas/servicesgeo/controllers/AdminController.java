0879b9d09512d2a469748df56591c91f85134ec2
Douglas Lundy <douglaslundy@gmail.com>
2018-11-30 14:48:48 -0200

    Pagina erro 404

--- src/main/java/br/com/dlsistemas/servicesgeo/controllers/AdminController.java
+++ src/main/java/br/com/dlsistemas/servicesgeo/controllers/AdminController.java
@@ -2,6 +2,7 @@ package br.com.dlsistemas.servicesgeo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
@@ -9,6 +10,11 @@ public class AdminController {
 	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/error")
+	public String erro404(){
+		return "error/404";
 	}
 
 }
